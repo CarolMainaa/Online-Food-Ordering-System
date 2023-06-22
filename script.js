53189b9b6380c36f7bb86eb39bf1a73a7a28ee5f
FindingNemoo <carolmaina502@gmail.com>
2023-06-21 22:20:21 -0700

    added order form

--- script.js
+++ script.js
@@ -30,6 +30,112 @@ const sr = ScrollReveal ({
   sr.reveal('.before',{ delay:350, origin:'bottom' })
   sr.reveal('.footer-logo',{ delay:350, origin:'bottom' })
 
+// form validation
+// Get the form element
+const form = document.querySelector('form');
+
+// Get the success message element
+const successMessage = document.getElementById('success-message');
+
+// Add event listener to the submit button
+form.addEventListener('submit', function(event) {
+  // Prevent the default form submission
+  event.preventDefault();
+
+  // Validate the form fields
+  const fullnameInput = document.getElementById('fullname');
+  const lastnameInput = document.getElementById('lastname');
+  const addressInput = document.getElementById('address');
+  const phoneInput = document.getElementById('phone');
+
+  // Check if the first name is empty
+  if (fullnameInput.value.trim() === '') {
+    showError(fullnameInput, 'First Name is a required field');
+    return;
+  }
+
+  // Check if the last name is empty
+  if (lastnameInput.value.trim() === '') {
+    showError(lastnameInput, 'Last Name is a required field');
+    return;
+  }
+
+  // Check if the address is empty
+  if (addressInput.value.trim() === '') {
+    showError(addressInput, 'Address is a required field');
+    return;
+  }
+
+  // Check if the phone number is empty
+  if (phoneInput.value.trim() === '') {
+    showError(phoneInput, 'Phone Number is a required field');
+    return;
+  }
+
+  // Check if the phone number is valid (optional)
+  const phoneRegex = /^[0-9]{10}$/;
+  if (!phoneRegex.test(phoneInput.value.trim())) {
+    showError(phoneInput, 'Please enter a valid phone number');
+    return;
+  }
+
+  // Optional: Check if any checkbox is selected
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  let isChecked = false;
+  for (const checkbox of checkboxes) {
+    if (checkbox.checked) {
+      isChecked = true;
+      break;
+    }
+  }
+  if (!isChecked) {
+    alert('Please select at least one extra filling');
+    return;
+  }
+
+  // Optional: Validate other fields as needed
+
+  // Simulate form submission success (Replace with your actual submission code)
+  // Here, we're simply displaying a success message and resetting the form
+  displaySuccessMessage();
+  form.reset();
+});
+
+// Function to display error message and apply red border
+function showError(inputElement, errorMessage) {
+  // Add red border
+  inputElement.style.border = '1px solid red';
+
+  // Create error message element
+  const errorElement = document.createElement('div');
+  errorElement.className = 'error-message';
+  errorElement.innerText = errorMessage;
+
+  // Check if error message element already exists
+  const existingError = inputElement.parentElement.querySelector('.error-message');
+  if (existingError) {
+    inputElement.parentElement.removeChild(existingError);
+  }
+
+  // Append error message element to the parent element
+  inputElement.parentElement.appendChild(errorElement);
+
+  // Set focus on the input field
+  inputElement.focus();
+}
+
+// Function to display success message
+function displaySuccessMessage() {
+  // Display success message
+  successMessage.textContent = 'Form submitted successfully!';
+  successMessage.classList.remove('hidden');
+
+  // Hide success message after 5 seconds
+  setTimeout(function() {
+    successMessage.textContent = '';
+    successMessage.classList.add('hidden');
+  }, 5000);
+}
 
 
 
@@ -82,21 +188,3 @@ const sr = ScrollReveal ({
 
 
 
-
-
-// let menubar = document.querySelector('#menubar');
-// let mynav = document.querySelector('.navbar');
-
-
-
-// menubar.onclick = () =>{
-//     menubar.classList.toggle('fa-times')
-//     mynav.classList.toggle('active')
-// }
-
-// document.querySelector('#loginbtn').onclick = () =>{
-//     document.querySelector('.login-form').classList.toggle('active');
-//   }
-//   document.querySelector('#closeloginform').onclick = () =>{
-//     document.querySelector('.login-form').classList.remove('active');
-//   }
